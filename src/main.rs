f5e89eaabdf01e701c5b1130d618e1196f4d6af2
Yucklys <yucklys687@outlook.com>
2025-04-20 18:14:49 -0500

    add Escape keyboard shortcut

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use gtk::gio::ActionEntry;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button};
+use gtk::{Application, ApplicationWindow};
 use gtk4_layer_shell::{Layer, LayerShell, KeyboardMode};
 
 const APP_ID: &str = "io.github.yucklys.focalpoint";
@@ -10,6 +11,8 @@ fn main() {
         .build();
 
     app.connect_activate(build_ui);
+
+		app.set_accels_for_action("win.close", &["Escape"]);
 
     app.run();
 }
@@ -24,7 +27,14 @@ fn build_ui(app: &Application) {
 
 		window.init_layer_shell();
 		window.set_layer(Layer::Top);
-		window.set_keyboard_mode(KeyboardMode::OnDemand);
+		window.set_keyboard_mode(KeyboardMode::Exclusive);
+
+		let action_close = ActionEntry::builder("close")
+				.activate(|window: &ApplicationWindow, _, _| {
+						window.close();
+				})
+				.build();
+		window.add_action_entries([action_close]);
 
     let label = gtk::Label::new(Some("Focal Point"));
     window.set_child(Some(&label));

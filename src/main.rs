a4a6ae33237840fe1ad43d8338218dad55279fba
Yucklys <yucklys687@outlook.com>
2025-04-20 19:09:59 -0500

    feat: setup EnvFilter for tracing_subscriber

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,13 @@ const APP_ID: &str = "io.github.yucklys.focalpoint";
 
 fn main() {
 		// Initialize tracing subscriber for logging
-		tracing_subscriber::fmt::init();
+		if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
+			tracing_subscriber::fmt()
+				.with_env_filter(env_filter)
+				.init();
+		} else {
+			tracing_subscriber::fmt().init();
+		}
 		
     let app = Application::builder()
         .application_id(APP_ID)
